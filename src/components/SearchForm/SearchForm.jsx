33354bd5245595930c4a5bef34dbc8154254aaef
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-29 21:03:19 +0300

    Search form added

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,35 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    search: ""
+  }
+
+  chageHebdler = (event) => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+  submitHendler = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.search);
+    this.setState({search: ""});
+  }
+
   render() {
-    return <h2>SearchForm</h2>;
+    return <SearchFormStyled onSubmit={this.submitHendler}>
+            <FormBtn type="submit">
+              <FiSearch size="16px" />
+            </FormBtn>
+            <InputSearch
+              onChange={this.chageHebdler}
+              placeholder="What do you want to write?"
+              name="search"
+              required
+              autoFocus
+              value={this.state.search}
+            />
+          </SearchFormStyled>
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,12 +1,19 @@
 import { Component } from 'react';
-
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    keyWord: ""
+  }
+
+  chageKeyword = word => {
+    this.setState({keyWord: word});
+  }
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.chageKeyword}/>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
